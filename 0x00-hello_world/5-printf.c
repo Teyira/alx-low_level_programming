6da276221c9c0d5cd44d8d1aae344875bc5f84c8
Teyira <teyirafg@gmail.com>
2022-06-30 14:49:59 +0100

    First Commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - to be called
+ *Return:0 for the int
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
