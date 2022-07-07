f804c785819dcaae8a000427ef5758ec6ffcc1de
Teyira <teyirafg@gmail.com>
2022-07-07 01:12:39 +0100

    Last fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,14 +12,14 @@ int main(void)
 	int i = 0;
 	unsigned long int a = 0, b = 1, next_fib = 0;
 
-	while (i < 98)
+	while (i < 99)
 	{
 		next_fib = a + b;
 		a = b;
 		b = next_fib;
 		printf("%lu", next_fib);
 
-		if (i < 97)
+		if (i < 98)
 			printf(", ");
 		i++;
 	}
