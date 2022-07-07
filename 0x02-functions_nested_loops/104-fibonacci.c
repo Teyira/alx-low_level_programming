0d2ee2da6a345a33b46542fdf6dc6ab3cb68c1eb
Teyira <teyirafg@gmail.com>
2022-07-07 01:03:28 +0100

    Alx task solutions

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
+ * pointers, array/tables, or structures
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i = 0;
+	unsigned long int a = 0, b = 1, next_fib = 0;
+
+	while (i < 98)
+	{
+		next_fib = a + b;
+		a = b;
+		b = next_fib;
+		printf("%lu", next_fib);
+
+		if (i < 97)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
