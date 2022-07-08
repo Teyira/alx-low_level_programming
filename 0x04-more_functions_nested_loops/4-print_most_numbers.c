beb24788271273722573acdfcddf764943ab4091
Teyira <teyirafg@gmail.com>
2022-07-08 02:13:14 +0100

    Initial commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,25 +1,17 @@
 #include "main.h"
 
-include "holberton.h"
-
 /**
- * print_most_numbers - prints numbers
- * Return:0
+ * print_most_numbers - print 0 to 9, skip 2 and 4
 */
 
 void print_most_numbers(void)
 {
-	char n = 0;
+	char i;
 
-	while (n <= 9)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (n != 2 && n != 4)
-		{
-			_putchar('0' + n);
-		}
-
-		n++;
+		if (i != '2' && i != '4')
+			_putchar(i);
 	}
 	_putchar('\n');
-
 }
