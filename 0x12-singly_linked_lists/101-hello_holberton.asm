05816fefe3ecabdf6699f55360ec64f756462db8
Teyira <teyirafg@gmail.com>
2022-08-12 00:00:22 +0100

    printf crash fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,12 @@
-; ----------------------------------------------------------------------------------------
-; Writes "Hello, Holberton" to the console using only system calls. Runs on 64-bit macOS only.
-; To assemble and run:
-;
-;     nasm -fmacho64 hello.asm && ld hello.o && ./a.out
-; ----------------------------------------------------------------------------------------
+.cstring
+_hello: .asciz "Hello, Holberton\n"
 
-          global    start
-
-          section   .text
-start:    mov       rax, 0x02000004         ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 0x02000001         ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
-
-          section   .data
-message:  db        "Hello, Holberton", 10      ; note the newline at the end
+.text
+.globl _main
+_main:
+    sub  $8, %rsp           // align rsp to 16B boundary
+    mov  $0, %rax
+    lea  _hello(%rip), %rdi // load address of format string
+    call _printf            // call printf
+    add  $8, %rsp           // restore rsp
+    ret
