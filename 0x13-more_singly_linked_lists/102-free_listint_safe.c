0782c1f27b71b3d4ed98f7ac3858e0dbd25bfccd
Teyira <teyirafg@gmail.com>
2022-08-16 23:42:51 +0100

    Initial commit

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ *free listint_t list - This function can free lists with a loop
+ * @head: the function sets the head to NULL
+ * Return: the size of the list that was free’d
+*/
+
